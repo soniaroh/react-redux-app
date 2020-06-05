37f83641cc0bdd820ce44fa3403bee97dd39f9ef
Neda Rohani <sonia@Nedas-MacBook-Pro.local>
2020-06-05 13:57:52 -0700

    add filtering capability

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { fetchUsers} from './actions/actionCreators';
 import { connect } from 'react-redux';
 import User from './components/User';
 
-
 const App = ({dispatch, users, loading, error}) => {
+  const [searchedTerm, setSearchedTerm] = useState('');
+  const [isFiltering, handleFilter] = useState(false);
 
   useEffect(() => {
     dispatch(fetchUsers())
   }, [dispatch]);
+
+  const handleChange = e => {
+    setSearchedTerm(e.target.value)
+  }
+
+  const filterUsers = (users, searchedTerm) => {
+    return users.filter(user => user.name.toLowerCase().includes(searchedTerm))
+  }
+
+  let filteredUsers = filterUsers(users, searchedTerm);
 
   if(loading) return <div>please wait ...</div>
   if(error) return <div>something unexpected happened, please try again</div>
   return (
     <div className="container-fluid">
       <h3 className="text-info">Users List</h3>
-      <span class="border-bottom-0"></span>
-      {users.map(user => {
-        return <User key={user.id} user={user}/>
-      })}
+      <input className="form-control" type="text" placeHolder="Search..." value={searchedTerm} onChange={handleChange}/>
+      <button type="submit" className="btn btn-primary mb-2" onClick={() => {handleFilter(true)}}>Submit</button>
+      {isFiltering && searchedTerm ? 
+      filteredUsers.map(user => <User key={user.id} user={user}/>) 
+      : 
+      users.map(user => <User key={user.id} user={user}/>)
+      }
     </div>
   );
 }
